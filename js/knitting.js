477a5fbfda7f1c76bdbedafcc19a05dc828cc46e
Matthew <matthewnfaulkner@gmail.com>
2017-08-04 15:54:21 +0100

    fixed js

--- js/knitting.js
+++ js/knitting.js
@@ -5,19 +5,20 @@ function makeSQL(){
 
 $(window).on('load', function(){
   $('form').submit(function(event){
+    event.preventDefault();
     var values = $(this).serialize();
     var colour = values.split("=")[1];
     var sql = "SELECT * FROM patterns"
     if(colour != "any"){
       sql += "WHERE colour=" + colour;
     }
-    $$.ajax({
+    $.ajax({
         type: "POST",
         url: '104.199.75.37/query.php',
         data: {'functionName': 'query', 'query': sql},
         success: function(response){
           console.log(response);
         }
-    event.preventDefault();
+      })
   })
 })
